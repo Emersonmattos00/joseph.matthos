78accc5efa59e7ca77f04f3d98480c3eea761b9a
Emerson Mattos <emerson.mattos00@gmail.com>
2026-09-21 14:56:57 -0300

    Delete api/gerar-hash.js

--- api/gerar-hash.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end('{"ok":true,"msg":"vivo"}');
-};test: gerar-hash versão mínima
