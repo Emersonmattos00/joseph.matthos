df1b1fc886358dd4a8d8b131336c040187983547
Emerson Mattos <emerson.mattos00@gmail.com>
2026-09-21 14:38:30 -0300

    Update gerar-hash.js

--- api/gerar-hash.js
+++ api/gerar-hash.js
@@ -4,4 +4,4 @@ module.exports = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end('{"ok":true,"msg":"vivo"}');
-};
+};test: gerar-hash versão mínima
