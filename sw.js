8541b285388b2a98fbb6d7440608927967dbc7a9
Emerson Mattos <emerson.mattos00@gmail.com>
2026-09-25 07:41:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
        3. req.headers.get('range')
    ============================================================ */
 
-const CACHE_VERSION = 'jm-v18';
+const CACHE_VERSION = 'jm-v19';
 
 // ─────────────────────────────────────────────────────────────
 // Precache — assets estáticos do site público
